7024ccef2cf8074eb01f14ab143461835cf70833
Romain Spenato <rspenato@gmail.com>
2015-11-17 13:41:00 +0100

    avancement page accueil

--- pages/accueil.php
+++ pages/accueil.php
@@ -1,3 +1,10 @@
-<br/><br/>
-Ceci est la page d'accueil
-<br/><br/><br/><br/>
+<div id="slogan_accueil">A BAG IS THE BEST SUBSTITUTE FOR MEN</div>
+<div id="search_accueil">
+	<h1>Search The One</h1>
+	<div id="select_story">
+		<select>
+			<option>Select a story</option>
+			<option>Intense, the trendy bag</option>
+		</select>
+	</div>
+</div>
