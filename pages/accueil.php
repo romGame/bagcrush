675339b3c20fe727f4433283faf6c299e04fec7d
Romain Spenato <rspenato@gmail.com>
2015-11-18 14:16:52 +0100

    avancement page accueil

--- pages/accueil.php
+++ pages/accueil.php
@@ -7,4 +7,23 @@
 			<option>Intense, the trendy bag</option>
 		</select>
 	</div>
+	<div id="story_img">
+		<img src="images/image_theme_sac.png" />
+	</div>
+</div>
+<div id="sign_up_event_block">
+	<div class="close_arrow">X</div>
+	<h1>Don't miss a chance to fall in love</h1>
+	<h2>Sign up for exclusive offers, exciting events and shots of inspiration</h2>
+	<form id="signup_accueil" method="post" action="">
+		<input id="text_signup_input" type="text" name="email_signup"></input>
+		<input id="submit_btn_accueil" type="image" name="submit" src="images/fleche_submit.png" border="0" alt="Submit" />
+	</form>
+	<div id="signup_conditions">
+		By giving your email address, you agree with our general conditions.<br/>
+		By pursuing your navigation on this website, you accept the use of cookies slowing to realize statistic of visit. <a href="#">More informations</a>
+	</div>
+</div>
+<div id="product_accueil_block">
+
 </div>
